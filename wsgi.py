6238decf083d4db1a6be201e8a1a67e45fc41faf
Parker Stephenson <parkerstephenson00@gmail.com>
2023-12-18 16:46:36 -0800

    Preparing for Heroku deployment
    Created Procfile and changed wsgi.py file to get ready for deployment

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,8 @@ app = create_app()
 
 # Check if the script is being run directly
 if __name__ == '__main__':
+    # Get the PORT environment variable from Heroku, defaulting to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    
     # Run the Flask app in debug mode with reloader turned off
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, port=port)
